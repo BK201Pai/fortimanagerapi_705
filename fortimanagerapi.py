dc0dae9f46e5567f23638620077f1f7140264bef
BK201Pai <visentin598@gmail.com>
2022-11-25 18:34:05 +0100

    Update fortimanagerapi.py

---
+++ fortimanagerapi.py
@@ -0,0 +1,32 @@
+import requests
+import json
+
+#ignore certificate
+from urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+class FortiManager:
+    def __init__(self, ip, user, password):
+        ipaddr = 'https://' + ip
+        #FMG json-rpc wants all calls on the same url
+        self.url = ipaddr + '/jsonrpc'
+        self.s = requests.Session()
+        payload = {"id": 1, "method": "exec","params": [{"data": {"user": user,"password": password},"url": "/sys/login/user"}]}
+        self.r = self.s.post(self.url, json=payload, verify=False)
+        print(f"login status {self.r.status_code}")
+        appSession = json.loads(self.r.text)
+        self.tknSession = appSession['session']
+    
+    def logout(self):
+        payload = {"id": 1, "method": "exec","params": [{"url": "/sys/logout"}], "session": self.tknSession}
+        req = self.s.post(self.url, json=payload, verify=False)
+        return req.status_code
+
+    def generatePayloadRequest(self, method, url, data, id=1):
+        payload = {"method": method, "params": [{"url": url, "data": {data}}], "id": id, "session": self.tknSession}
+        req = self.s.post(self.url, json=payload, verify=False)
+        return req
+
+    def getAllTask():
+        generatePayloadRequest("get", "/task/task", {})
