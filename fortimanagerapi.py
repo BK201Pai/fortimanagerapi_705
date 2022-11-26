c85b0b94c43499b0f08a871bb925fd69514238c8
BK201Pai <visentin598@gmail.com>
2022-11-26 18:46:41 +0100

    Update fortimanagerapi.py

--- fortimanagerapi.py
+++ fortimanagerapi.py
@@ -8,9 +8,8 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 class FortiManager:
     def __init__(self, ip, user, password):
-        ipaddr = 'https://' + ip
         #FMG json-rpc wants all calls on the same url
-        self.url = ipaddr + '/jsonrpc'
+        self.url = 'https://' + ip + '/jsonrpc'
         self.s = requests.Session()
         payload = {"id": 1, "method": "exec","params": [{"data": {"user": user,"password": password},"url": "/sys/login/user"}]}
         self.r = self.s.post(self.url, json=payload, verify=False)
@@ -24,7 +23,7 @@ class FortiManager:
         return req.status_code
 
     def generatePayloadRequest(self, method, url, data, id=1):
-        payload = {"method": method, "params": [{"url": url, "data": {data}}], "id": id, "session": self.tknSession}
+        payload = {"method": method, "params": [{"url": url, "data": data}], "id": id, "session": self.tknSession}
         req = self.s.post(self.url, json=payload, verify=False)
         return req
 
@@ -32,10 +31,10 @@ class FortiManager:
         req = self.generatePayloadRequest("get", "/task/task", data, id)
         print(f"return status {req.status_code}")
         app = json.loads(req.text)
-        return app['data']
+        return app['result']
 
-    def getTaskByID(self, id=1, data={}):
-        req = self.generatePayloadRequest("get", f"/task/task/{id}", data, id)
+    def getTaskByID(self, TaskID, id=1, data={}):
+        req = self.generatePayloadRequest("get", f"/task/task/{TaskID}", data, id)
         print(f"return status {req.status_code}")
         app = json.loads(req.text)
-        return app['data']
+        return app['result']
