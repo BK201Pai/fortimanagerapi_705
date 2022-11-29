56b154a1ebcd2dac238955321bd51f1ff9bd40e7
BK201Pai <visentin598@gmail.com>
2022-11-29 11:23:47 +0100

    Fixed query params + docs
    Revised query params for Filters and Fields in the query.
    Generated docs.

--- fortimanagerapi.py
+++ fortimanagerapi.py
@@ -1,6 +1,5 @@
 import requests
 import json
-
 from urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)  # type: ignore
 
@@ -9,17 +8,22 @@ class FortiManager:
         #FMG json-rpc wants all calls on the same url
         self.url = 'https://' + ip + '/jsonrpc'
         self.s = requests.Session()
+        #static payload login as it is always the same
         payload = {"id": 1, "method": "exec","params": [{"data": {"user": user,"password": password},"url": "/sys/login/user"}]}
         self.r = self.s.post(self.url, json=payload, verify=False)
         print(f"login status {self.r.status_code}")
         appSession = json.loads(self.r.text)
         self.tknSession = appSession['session']
-    
+        
+    #static payload logout as it is always the same
     def logout(self):
         payload = {"id": 1, "method": "exec","params": [{"url": "/sys/logout"}], "session": self.tknSession}
         req = self.s.post(self.url, json=payload, verify=False)
         return req.status_code
 
+    #Since FMG wants all requests on the same url with post as per json-rpc standard,
+    #the method and the url is specified in the request json body
+    #params is a dict for additional filters or fields used by the query, url is contained inside of params
     def generatePayloadRequest(self, method, url, params={}, id=1):
         query = {}
         params['url'] = url
@@ -40,3 +44,4 @@ class FortiManager:
         req = self.generatePayloadRequest("get", f"/task/task/{TaskID}", params, id)
         app = json.loads(req.text)
         return app['result']
+
